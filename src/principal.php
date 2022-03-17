e0195e9ae3667deea526035789bd3e5dd4e9763b
Jorgeacero9 <jorgeacero9@gmail.com>
2022-03-16 23:56:04 -0500

    cards y margen top

--- src/principal.php
+++ src/principal.php
@@ -1,53 +1,140 @@
 <section>
-<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="4" aria-label="Slide 5"></button>
+    <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="4" aria-label="Slide 5"></button>
+        </div>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="./public/Img/avengers.jpg" width="100" height="400" class="d-block w-100" alt="...">
+                        <div class="carousel-caption d-none d-md-block">
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="./public/Img/star.jpg" width="100" height="400"  class="d-block w-100" alt="...">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>STAR WARS VII</h5>
+                        </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="./public/Img/sonic.jpg" width="100" height="400"  class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>SONIC</h5>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="./public/Img/uncharted.jpg" width="100" height="400"  class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>UNCHARTED</h5>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="./public/Img/BATMAN.jpg" width="100" height="400"  class="d-block w-100" alt="...">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>THE BATMAN</h5>
+                        </div>
+                </div>
 
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+    </div>
 
-</div>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="./public/Img/avengers.jpg" width="100" height="400" class="d-block w-100" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-      </div>
-    </div>
-    <div class="carousel-item">
-      <img src="./public/Img/star.jpg" width="100" height="400"  class="d-block w-100" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>STAR WARS VII</h5>
-      </div>
-    </div>
-    <div class="carousel-item">
-      <img src="./public/Img/sonic.jpg" width="100" height="400"  class="d-block w-100" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>SONIC</h5>
-      </div>
-    </div>
-    <div class="carousel-item">
-      <img src="./public/Img/uncharted.jpg" width="100" height="400"  class="d-block w-100" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>UNCHARTED</h5>
-      </div>
-    </div>
-    <div class="carousel-item">
-      <img src="./public/Img/BATMAN.jpg" width="100" height="400"  class="d-block w-100" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>THE BATMAN</h5>
-      </div>
-
-  </div>
-
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
 </section>
+<section>
+<h1><span class="badge badge-secondary">PELICULAS</span></h1>
+</section>
+
+<section class="Peliculas">
+    <div class="container">
+        <div class="row">
+        
+            <div class="col-md-3">
+                <div class="card" style="width: 14rem;">
+                    <img class="card-img-top" src="./public/Img/godzilla.jpg" alt="Card image cap"  height="250">
+                    <div class="card-body">
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+
+                <div class="col-md-3">
+                <div class="card" style="width: 14rem;">
+                    <img class="card-img-top" src="./public/Img/hobbit.jpg" alt="Card image cap" height="250" >
+                        <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card" style="width: 14rem;">
+                        <img class="card-img-top" src="./public/Img/red.jpg" alt="Card image cap" height="250">
+                            <div class="card-body">
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card" style="width: 14rem;">
+                        <img class="card-img-top" src="./public/Img/woman.jpg" alt="Card image cap" height="250">
+                        <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+
+        </div>
+
+
+        <div class="row mt-4">
+        
+        <div class="col-md-3">
+            <div class="card" style="width: 14rem;">
+                <img class="card-img-top" src="./public/Img/godzilla.jpg" alt="Card image cap"  height="250">
+                <div class="card-body">
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+        </div>
+
+            <div class="col-md-3">
+            <div class="card" style="width: 14rem;">
+                <img class="card-img-top" src="./public/Img/hobbit.jpg" alt="Card image cap" height="250" >
+                    <div class="card-body">
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card" style="width: 14rem;">
+                    <img class="card-img-top" src="./public/Img/red.jpg" alt="Card image cap" height="250">
+                        <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card" style="width: 14rem;">
+                    <img class="card-img-top" src="./public/Img/woman.jpg" alt="Card image cap" height="250">
+                    <div class="card-body">
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+
+    </div>
+
+    </div>
+</section>
+
+
+
+
