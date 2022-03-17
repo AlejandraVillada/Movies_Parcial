61ea52841f215ffbb19927a3486961669565b072
AlejandraVillada <villadatorresmariaalejandra@gmail.com>
2022-03-17 15:15:18 -0500

    series

--- src/informacion.php
+++ src/informacion.php
@@ -1,1 +1,80 @@
-<Strong>HOLA</Strong>
+<div class="container  text-white" style="min-height:600px;">
+
+    <div>
+        <h1>Series </h1>
+    </div>
+
+    <div class="row">
+        <div class="col-md-3">
+            <div class="card" style="width: 14rem;">
+                <img class="card-img-top" src="./public/Img/riverdale.png" alt="Card image cap" height="250">
+                <div class="card-body">
+                    <p class="card-text">Riverdale.</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-3">
+            <div class="card" style="width: 14rem;">
+                <img class="card-img-top" src="./public/Img/dr_house.png" alt="Card image cap" height="250">
+                <div class="card-body">
+                    <p class="card-text">Dr. House.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card" style="width: 14rem;">
+                <img class="card-img-top" src="./public/Img/GOT.png" alt="Card image cap" height="250">
+                <div class="card-body">
+                    <p class="card-text">Game of Thrones.</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-3">
+            <div class="card" style="width: 14rem;">
+                <img class="card-img-top" src="./public/Img/the_crown.png" alt="Card image cap" height="250">
+                <div class="card-body">
+                    <p class="card-text">The Crown.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row mt-3">
+        <div class="col-md-3">
+            <div class="card" style="width: 14rem;">
+                <img class="card-img-top" src="./public/Img/la_casa_papel.png" alt="Card image cap" height="250">
+                <div class="card-body">
+                    <p class="card-text">La Casa de Papel.</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-3">
+            <div class="card" style="width: 14rem;">
+                <img class="card-img-top" src="./public/Img/narcos.png" alt="Card image cap" height="250">
+                <div class="card-body">
+                    <p class="card-text">Narcos.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card" style="width: 14rem;">
+                <img class="card-img-top" src="./public/Img/greys_anatomy.png" alt="Card image cap" height="250">
+                <div class="card-body">
+                    <p class="card-text">Grey's Anatomy.</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-3">
+            <div class="card" style="width: 14rem;">
+                <img class="card-img-top" src="./public/Img/black_mirror.png" alt="Card image cap" height="250">
+                <div class="card-body">
+                    <p class="card-text">Black Mirror</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
