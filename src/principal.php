8c702501a48e01e704d0dacfa53deaac24262083
Jorgeacero9 <jorgeacero9@gmail.com>
2022-03-16 22:04:23 -0500

    slider de peliculas

--- src/principal.php
+++ src/principal.php
@@ -1,10 +1,47 @@
-<section id="movies">
-    <div class="row">
-        <div class="col">
-            <div class="card">a</div>
-        </div>
-        <div class="col"></div>
-        <div class="col"></div>
-        <div class="col"></div>
+<section>
+<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
+
+</div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="./public/Img/thor.jpg" width="100" height="400" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>THOR RAGNAROK</h5>
+      </div>
     </div>
+    <div class="carousel-item">
+      <img src="./public/Img/star.jpg" width="100" height="400"  class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>STAR WARS VII</h5>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="./public/Img/sonic.jpg" width="100" height="400"  class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>SONIC</h5>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="./public/Img/uncharted.jpg" width="100" height="400"  class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>UNCHARTED</h5>
+      </div>
+    </div>
+
+  </div>
+
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
 </section>
